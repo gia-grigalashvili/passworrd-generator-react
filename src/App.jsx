3997342e3da247331b9cd0cffddb310dc8c57fd2
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-18 01:39:43 +0400

    made password's style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import Name from "./Components/Name";
 import Copyheader from "./Components/Copyheader";
+import styled from "styled-components";
 function App() {
   return (
     <>
-      <Name />
-      <Copyheader />
+      <Container>
+        <Name />
+        <Copyheader />
+      </Container>
     </>
   );
 }
-
+const Container = styled.div`
+  width: 343px;
+  margin-top: 100px;
+`;
 export default App;
--- src/Components/Copyheader.jsx
+++ src/Components/Copyheader.jsx
@@ -1,15 +1,27 @@
-import React from "react";
 import styled from "styled-components";
 function Copuheader() {
-  return <Copypass></Copypass>;
+  return (
+    <Copypass>
+      <h2 className="password">gia</h2>
+    </Copypass>
+  );
 }
 const Copypass = styled.div`
   display: flex;
+  margin-top: 20px;
   align-items: center;
   justify-content: space-between;
   background-color: #24232c;
   color: #e6e5ea;
-  padding: 16px 15px;
+  padding: 14px 15px;
   margin-bottom: 16px;
+  .password {
+    font-family: "JetBrains Mono";
+    font-size: 24px;
+    line-height: 32px;
+    mix-blend-mode: normal;
+    opacity: 0.25;
+    color: #f8f8f8;
+  }
 `;
 export default Copuheader;
