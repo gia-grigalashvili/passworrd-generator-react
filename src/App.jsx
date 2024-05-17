1a27d3730cb0b199fe3c2d3b4fa1e12e82eac9e8
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-18 01:32:35 +0400

    import copyheader in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Name from "./Components/Name";
+import Copyheader from "./Components/Copyheader";
 function App() {
   return (
     <>
       <Name />
+      <Copyheader />
     </>
   );
 }
