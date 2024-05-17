c2fd2793dacdb61cc84a872ed09918ecc09c5373
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-18 02:07:03 +0400

    import maincont in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Name from "./Components/Name";
 import Copyheader from "./Components/Copyheader";
 import styled from "styled-components";
+import Maincont from "./Components/Maincont";
 function App() {
   return (
     <>
       <Container>
         <Name />
         <Copyheader />
+        <Maincont />
       </Container>
     </>
   );
---
+++ src/Components/Maincont.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+function Maincont() {
+  return <Mainconts>Main-cont</Mainconts>;
+}
+const Mainconts = styled.div`
+  background-color: #24232c;
+  padding: 21px 16px 16px;
+`;
+export default Maincont;
